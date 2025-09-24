5067c7428113a2914fd770ab64542e2e0a12f345
nazwastpn <nazwasetiapin@gmail.com>
2025-09-25 00:26:50 +0700

    mengubah tampilan topbar

--- keuangan-app/resources/views/layouts/topbar.blade.php
+++ keuangan-app/resources/views/layouts/topbar.blade.php
@@ -1,27 +1,29 @@
 <!-- layouts/topbar.blade.php -->
-<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+<nav class="navbar navbar-expand navbar-light topbar mb-4 static-top shadow"
+  style="background: linear-gradient(90deg, #ffffffff 0%, #e3effb 65%, #d1e6f9 100%);">
+
 
   <!-- Tombol Menu (Garis Tiga) di kiri -->
-  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+  <button id="sidebarToggle" class="btn btn-link rounded-circle mr-3">
     <i class="fas fa-bars"></i>
   </button>
 
-  <!-- Spacer biar kanan & kiri terpisah -->
+  <!-- Spacer -->
   <div class="ml-auto"></div>
 
   <!-- Bagian kanan (Notifikasi + User Info) -->
-  <ul class="navbar-nav">
+  <ul class="navbar-nav align-items-center">
 
     <!-- Notifikasi -->
     <li class="nav-item dropdown no-arrow mx-1">
       <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown"
         aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-bell fa-fw"></i>
+        <i class="fas fa-bell fa-fw text-primary"></i>
         <span class="badge badge-danger badge-counter">3+</span>
       </a>
-      <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+      <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--fade-in"
         aria-labelledby="alertsDropdown">
-        <h6 class="dropdown-header">Notifikasi</h6>
+        <h6 class="dropdown-header bg-primary text-white">Notifikasi</h6>
         <a class="dropdown-item d-flex align-items-center" href="#">
           <div class="mr-3">
             <div class="icon-circle bg-primary">
@@ -43,31 +45,18 @@
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
         aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+        <span class="mr-2 d-none d-lg-inline text-dark small">
           {{ Auth::user()->name ?? 'Guest' }}
         </span>
-        <img class="img-profile rounded-circle"
-          src="https://ui-avatars.com/api/?name={{ urlencode(Auth::user()->name ?? 'Guest') }}&background=random" />
+        <img class="img-profile rounded-circle border"
+          src="https://ui-avatars.com/api/?name={{ urlencode(Auth::user()->name ?? 'Guest') }}&background=0D8ABC&color=fff" />
       </a>
-      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+      <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="userDropdown">
         <a class="dropdown-item" href="{{ url('profile') }}">
           <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
           Profil
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-          Pengaturan
-        </a>
-        <div class="dropdown-divider"></div>
-        <form method="POST" action="{{ route('logout') }}">
-          @csrf
-          <button class="dropdown-item" type="submit">
-            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-            Logout
-          </button>
-        </form>
       </div>
     </li>
-
   </ul>
 </nav>
